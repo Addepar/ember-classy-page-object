ce848e357e9cf9a8428b635d9f570592179d6d68
Billy Littlefield <billy.littlefield@addepar.com>
2017-12-13 15:10:39 -0800

    refactor deepMergeDescriptors

--- addon-test-support/-private/utils/deep-merge-descriptors.js
+++ addon-test-support/-private/utils/deep-merge-descriptors.js
@@ -1,23 +1,29 @@
+function isObject(obj) {
+  return typeof obj === 'object' && obj !== null;
+}
+
 export default function deepMergeDescriptors(dest, src) {
   Object.getOwnPropertyNames(src).forEach((name) => {
     // Copy descriptor
-    let descriptor = Object.getOwnPropertyDescriptor(src, name)
+    let descriptor = Object.getOwnPropertyDescriptor(src, name);
+    const { value: srcValue } = descriptor;
 
-    if (Object.hasOwnProperty.call(dest, name)) {
-      const { value: destValue } = Object.getOwnPropertyDescriptor(dest, name);
-      const { value: srcValue } = descriptor;
+    if (isObject(srcValue)) {
+      let mergeTarget = {};
 
-      if (
-        typeof destValue === 'object' && destValue !== null
-        && typeof srcValue === 'object' && srcValue !== null
-      ) {
-        descriptor.value = deepMergeDescriptors(destValue, srcValue);
-      } else {
-        return;
+      if (Object.hasOwnProperty.call(dest, name)) {
+        let { value: destValue } = Object.getOwnPropertyDescriptor(dest, name);
+
+        mergeTarget = isObject(destValue) ? destValue : mergeTarget;
       }
+
+      descriptor.value = deepMergeDescriptors(mergeTarget, srcValue);
+
+    } else if (Object.hasOwnProperty.call(dest, name)) {
+      return;
     }
 
-    Object.defineProperty(dest, name, descriptor)
+    Object.defineProperty(dest, name, descriptor);
   });
 
   return dest;
