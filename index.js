b1d9a2abef5a3b682b4f881fd49db85dbb776425
Cory Forsyth <cory.forsyth@addepar.com>
2024-12-31 15:19:31 +0100

    Upgrades to fix ci

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 /* eslint-env node */
-'use strict';
+"use strict";
 
 module.exports = {
-  name: 'ember-classy-page-object',
+  name: "ember-classy-page-object",
 
   treeForAddonTestSupport(tree) {
     // intentionally not calling _super here
     // so that can have our `import`'s be
     // import { PageObject } from 'ember-classy-page-object';
 
-    const Funnel = require('broccoli-funnel');
+    const Funnel = require("broccoli-funnel");
 
     let namespacedTree = new Funnel(tree, {
-      srcDir: '/',
+      srcDir: "/",
       destDir: `/${this.moduleName()}`,
       annotation: `Addon#treeForTestSupport (${this.name})`,
     });
 
-    return this.preprocessJs(namespacedTree, '/', this.name, {
+    return this.preprocessJs(namespacedTree, "/", this.name, {
       registry: this.registry,
     });
-  }
+  },
 };
