aaea90f9e4895b73c5e969fcc0eb597bb62b9893
Cory Forsyth <cory.forsyth@addepar.com>
2025-01-02 15:13:51 +0100

    breaking: drop support for ember before 3.28

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,110 +1,54 @@
 /* eslint-env node */
-const getChannelURL = require('ember-source-channel-url');
+const getChannelURL = require("ember-source-channel-url");
 
-module.exports = function() {
+module.exports = function () {
   return Promise.all([
-    getChannelURL('release'),
-    getChannelURL('beta'),
-    getChannelURL('canary'),
-  ]).then(urls => {
+    getChannelURL("release"),
+    getChannelURL("beta"),
+    getChannelURL("canary"),
+  ]).then((urls) => {
     return {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.18',
+          name: "ember-lts-3.28",
           npm: {
             devDependencies: {
-              'ember-source': '~2.18.0',
+              "ember-source": "~3.28.0",
             },
           },
         },
         {
-          name: 'ember-lts-3.4',
+          name: "ember-release",
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0',
+              "ember-source": urls[0],
             },
           },
         },
         {
-          name: 'ember-lts-3.8',
+          name: "ember-beta",
           npm: {
             devDependencies: {
-              'ember-source': '~3.8.0',
+              "ember-source": urls[1],
             },
           },
         },
         {
-          name: 'ember-lts-3.12',
+          name: "ember-canary",
           npm: {
             devDependencies: {
-              'ember-source': '~3.12.0',
+              "ember-source": urls[2],
             },
           },
         },
         {
-          name: 'ember-lts-3.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.16.0',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-3.20',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.20.0',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-3.24',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.24.0',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-3.28',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.28.0',
-            },
-          },
-        },
-        {
-          name: 'ember-release',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[0],
-            },
-          },
-        },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[1],
-            },
-          },
-        },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[2],
-            },
-          },
-        },
-        {
-          name: 'ember-default',
+          name: "ember-default",
           npm: {
             devDependencies: {},
           },
         },
-      ]
+      ],
     };
   });
 };
