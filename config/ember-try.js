45fa3522f9f89333214c0e34907c3dafef65e546
Cory Forsyth <cory.forsyth@addepar.com>
2025-01-03 10:16:02 +0100

    support ember 4.4 lts

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
 const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = function() {
   return Promise.all([
@@ -10,6 +11,8 @@ module.exports = function() {
     return {
       useYarn: true,
       scenarios: [
+        embroiderSafe(),
+        embroiderOptimized(),
         {
           name: 'ember-lts-3.28',
           npm: {
@@ -19,7 +22,55 @@ module.exports = function() {
           },
         },
         {
-          name: 'ember-release',
+          name: "ember-lts-4.4",
+          npm: {
+            devDependencies: {
+              "ember-source": "~4.4.5",
+            },
+          },
+        },
+        {
+          name: "ember-lts-4.8",
+          npm: {
+            devDependencies: {
+              "ember-source": "~4.8.0",
+            },
+          },
+        },
+        {
+          name: "ember-lts-4.12",
+          npm: {
+            devDependencies: {
+              "ember-source": "~4.12.0",
+            },
+          },
+        },
+        {
+          name: "ember-lts-5.4",
+          npm: {
+            devDependencies: {
+              "ember-source": "~5.4.0",
+            },
+          },
+        },
+        {
+          name: "ember-lts-5.8",
+          npm: {
+            devDependencies: {
+              "ember-source": "~5.8.0",
+            },
+          },
+        },
+        {
+          name: "ember-lts-5.12",
+          npm: {
+            devDependencies: {
+              "ember-source": "~5.12.0",
+            },
+          },
+        },
+        {
+          name: "ember-release",
           npm: {
             devDependencies: {
               'ember-source': urls[0],
