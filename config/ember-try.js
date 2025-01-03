f853f93e4dc4265d4e1aab5f1006043f7117890d
Cory Forsyth <cory.forsyth@addepar.com>
2025-01-03 11:18:11 +0100

    support ember 5.8 lts

--- config/ember-try.js
+++ config/ember-try.js
@@ -51,6 +51,14 @@ module.exports = function() {
           },
         },
         {
+          name: "ember-lts-5.8",
+          npm: {
+            devDependencies: {
+              "ember-source": "~5.8.0",
+            },
+          },
+        },
+        {
           name: "ember-release",
           npm: {
             devDependencies: {
