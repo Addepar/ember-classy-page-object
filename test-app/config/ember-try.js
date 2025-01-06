5a36b117953051d3c7cc38679c173fcec1770f20
Cory Forsyth <cory.forsyth@addepar.com>
2025-01-06 12:09:24 +0100

    add missing ember-try

---
+++ test-app/config/ember-try.js
@@ -0,0 +1,54 @@
+/* eslint-env node */
+const getChannelURL = require('ember-source-channel-url');
+
+module.exports = function() {
+  return Promise.all([
+    getChannelURL('release'),
+    getChannelURL('beta'),
+    getChannelURL('canary'),
+  ]).then(urls => {
+    return {
+      useYarn: true,
+      scenarios: [
+        {
+          name: 'ember-lts-3.28',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.28.0',
+            },
+          },
+        },
+        {
+          name: 'ember-release',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[0],
+            },
+          },
+        },
+        {
+          name: 'ember-beta',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[1],
+            },
+          },
+        },
+        {
+          name: 'ember-canary',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[2],
+            },
+          },
+        },
+        {
+          name: 'ember-default',
+          npm: {
+            devDependencies: {},
+          },
+        },
+      ]
+    };
+  });
+};
